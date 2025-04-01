854e76a412f1814a2d527794ddfb83ac0adf1bbb
xXJGJUNIOR <157996049+xXJGJUNIOR@users.noreply.github.com>
2025-04-01 20:33:48 -0300

    Subi

---
+++ Envelope.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const envelope = document.querySelector(".envelope");
+
+    envelope.addEventListener("click", () => {
+        envelope.classList.toggle("open");
+    });
+});
